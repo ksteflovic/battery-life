6ff9c2756843bc537b5cf6a7c1cdf550810be4d2
ksteflovic <k.steflovic@gmail.com>
2022-12-11 03:31:59 +0100

    initBattery()

---
+++ js/app.js
@@ -0,0 +1,7 @@
+initBattery();
+
+function initBattery(){
+    const batteryLiquid = document.querySelector(".wave"),
+          batteryStatus = document.querySelector(".battery__status"),
+          batteryPercentage = 
+}
